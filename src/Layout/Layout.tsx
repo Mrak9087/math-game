f5c14b4fd06cac88d9c80f2cee80b38a5a7e7e6e
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2022-08-29 13:50:02 +0400

    feat: delete no used imports

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Card from '../components/Card';
-import Game from '../pages/Game';
 import Header from '../components/Header';
 
 const Layout = () => {
